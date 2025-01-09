13b9230ca66611e192cca7947da8236515745ea4
Richasy <Thansy@foxmail.com>
2025-01-09 19:30:30 +0800

    完善阿里云翻译

--- src/Connectors/Connectors.Ali/AliTranslationService.cs
+++ src/Connectors/Connectors.Ali/AliTranslationService.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using Richasy.AgentKernel.Connectors.Ali.Models;
-using Richasy.AgentKernel.Connectors.Ali.Models.Translation;
 using Richasy.AgentKernel.Models;
 using Richasy.AgentKernel.Translation;
 using RichasyKernel;
@@ -25,8 +24,7 @@ public sealed class AliTranslationService : ITextTranslationService, IHtmlTransl
     /// <summary>
     /// Initializes a new instance of the <see cref="AliTranslationService"/> class.
     /// </summary>
-    public AliTranslationService()
-        => _httpClient = HttpExtensions.CreateHttpClient();
+    public AliTranslationService() => _httpClient = HttpExtensions.CreateHttpClient();
 
     /// <inheritdoc/>
     public TranslationServiceConfig? Config => _config;
@@ -66,16 +64,16 @@ public sealed class AliTranslationService : ITextTranslationService, IHtmlTransl
         }
 
         var text = sourceContent.Replace("\r", "\n", StringComparison.InvariantCultureIgnoreCase).Replace("\n\n", "\n", StringComparison.InvariantCultureIgnoreCase);
-        var request = new AliTranslateRequest()
+
+        var dict = new Dictionary<string, string>
         {
-            FormatType = isHtml ? "html" : "text",
-            SourceLanguage = options?.SourceLanguage ?? "auto",
-            TargetLanguage = options?.TargetLanguage ?? "en",
-            SourceText = text,
+            { "FormatType", isHtml ? "html" : "text" },
+            { "SourceText",  text},
+            { "SourceLanguage", options?.SourceLanguage ?? "auto" },
+            { "TargetLanguage", options?.TargetLanguage ?? "en" },
         };
 
-        var requestJson = JsonSerializer.Serialize(request, JsonGenContext.Default.AliTranslateRequest);
-        using var httpRequest = CreateRequest(_apiEndpoint, requestJson, _config.AccessKey, _config.Secret);
+        using var httpRequest = CreateRequest(_apiEndpoint, dict, _config.AccessKey, _config.Secret);
         using var response = await _httpClient.SendAsync(httpRequest, cancellationToken).ConfigureAwait(false);
         var responseContent = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
         response.EnsureSuccessStatusCode();
@@ -86,32 +84,32 @@ public sealed class AliTranslationService : ITextTranslationService, IHtmlTransl
             Id = responseObj!.RequestId,
             Result = responseObj!.Data!.Translated!,
             SourceContent = sourceContent,
-            SourceLanguage = responseObj!.Data.DetectedLangauge,
+            SourceLanguage = responseObj!.Data.DetectedLangauge ?? options?.TargetLanguage,
             TargetLanguage = options?.TargetLanguage!,
         };
     }
 
     // 计算MD5并进行BASE64编码
-    private static string? MD5Base64(string s)
+    private static string? ComputeHashedPayload(string payload)
     {
-        if (s == null) return null;
-        string encodeStr;
-        var utfBytes = Encoding.UTF8.GetBytes(s);
-#pragma warning disable CA5351 // 不要使用损坏的加密算法
-        var md5Bytes = MD5.HashData(utfBytes);
-#pragma warning restore CA5351 // 不要使用损坏的加密算法
-        encodeStr = Convert.ToBase64String(md5Bytes);
-        return encodeStr;
+        var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(payload));
+        return Convert.ToHexStringLower(bytes);
     }
 
-    // 计算HMAC-SHA1
-    private static string HMACSha1(string data, string key)
+    // HMAC-SHA256加密
+    private static string HMACSha256(string data, string key)
     {
-#pragma warning disable CA5350 // 不要使用弱加密算法
-        using var hmacsha1 = new HMACSHA1(Encoding.UTF8.GetBytes(key));
-#pragma warning restore CA5350 // 不要使用弱加密算法
-        var rawHmac = hmacsha1.ComputeHash(Encoding.UTF8.GetBytes(data));
-        return Convert.ToBase64String(rawHmac);
+        var encoding = new UTF8Encoding();
+        var keyByte = encoding.GetBytes(key);
+        var dataBytes = encoding.GetBytes(data);
+        var hash = HMACSHA256.HashData(keyByte, dataBytes);
+        var hex = new StringBuilder(hash.Length * 2);
+        foreach (var b in hash)
+        {
+            hex.AppendFormat(CultureInfo.InvariantCulture, "{0:x2}", b);
+        }
+
+        return hex.ToString();
     }
 
     // 获取GMT时间
@@ -119,45 +117,64 @@ public sealed class AliTranslationService : ITextTranslationService, IHtmlTransl
         => date.ToUniversalTime().ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'", System.Globalization.CultureInfo.InvariantCulture);
 
     // 发送POST请求
-    private static HttpRequestMessage CreateRequest(string url, string body, string ak_id, string ak_secret)
+    private static HttpRequestMessage CreateRequest(string url, Dictionary<string, string> formData, string ak_id, string ak_secret)
     {
         var realUrl = new Uri(url);
         // HTTP header 参数
         var method = "POST";
         var accept = "application/json";
-        var content_type = "application/json";
-        var path = realUrl.AbsolutePath;
+        var content_type = "application/x-www-form-urlencoded";
         var now = DateTimeOffset.Now;
         var date = ToGMTString(now);
         var host = realUrl.Host;
+        var path = realUrl.AbsolutePath;
+        // Convert formData to string.
+        var body = string.Join("&", formData.Select(x => $"{x.Key}={Uri.EscapeDataString(x.Value)}"));
         // 1. 对body做MD5+BASE64加密
-        var bodyMd5 = MD5Base64(body);
+        var hashedBody = ComputeHashedPayload(body);
+        System.Diagnostics.Debug.WriteLine("hashedBody: \n" + hashedBody);
         var uuid = Guid.NewGuid().ToString();
-        var stringToSign = method + "\n" + accept + "\n" + bodyMd5 + "\n" + content_type + "\n" + date + "\n"
-            + "x-acs-action:TranslateGeneral\n"
-            + "x-acs-signature-method:HMAC-SHA1\n"
-                              + "x-acs-signature-nonce:" + uuid + "\n"
-                              + "x-acs-signature-version:1.0\n"
-                              + "x-acs-version:2018-10-12\n"
-                              + path + "\n";
+
+        var canonicalHeaders =
+            $"content-type:{content_type}\n" +
+            $"host:{host}\n" +
+            $"x-acs-action:TranslateGeneral\n" +
+            $"x-acs-content-sha256:{hashedBody}\n" +
+            $"x-acs-date:{now.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss'Z'", CultureInfo.InvariantCulture)}\n" +
+            $"x-acs-signature-nonce:{uuid}\n" +
+            $"x-acs-version:2018-10-12\n";
+        System.Diagnostics.Debug.WriteLine("canonicalHeaders: \n" + canonicalHeaders);
+        var signedHeaders = "content-type;host;x-acs-action;x-acs-content-sha256;x-acs-date;x-acs-signature-nonce;x-acs-version";
+        System.Diagnostics.Debug.WriteLine("signedHeaders: \n" + signedHeaders);
+        var canonicalRequest =
+            method + "\n" +
+            path + "\n\n" +
+            canonicalHeaders + "\n" +
+            signedHeaders + "\n" +
+            hashedBody;
+        System.Diagnostics.Debug.WriteLine("canonicalRequest: \n" + canonicalRequest);
+        var hashedCanonicalRequest = ComputeHashedPayload(canonicalRequest);
+        var stringToSign =
+            "ACS3-HMAC-SHA256\n" + hashedCanonicalRequest;
+        System.Diagnostics.Debug.WriteLine("stringToSign: \n" + stringToSign);
         // 2. 计算 HMAC-SHA1
-        var signature = HMACSha1(stringToSign, ak_secret);
+        var signature = HMACSha256(stringToSign, ak_secret);
+        System.Diagnostics.Debug.WriteLine("signature: \n" + signature);
         // 3. 得到 authorization header
-        var authHeader = "acs " + ak_id + ":" + signature;
+        var authHeader = $"ACS3-HMAC-SHA256 Credential={ak_id},SignedHeaders={signedHeaders},Signature={signature}";
+        System.Diagnostics.Debug.WriteLine("authHeader: \n" + authHeader);
 
         var request = new HttpRequestMessage(HttpMethod.Post, realUrl);
         request.Headers.TryAddWithoutValidation("Accept", accept);
-        request.Headers.TryAddWithoutValidation("Content-Type", content_type);
-        request.Headers.Date = now;
+        request.Headers.Date = now.ToUniversalTime();
         request.Headers.TryAddWithoutValidation("Host", host);
         request.Headers.TryAddWithoutValidation("Authorization", authHeader);
-        request.Headers.TryAddWithoutValidation("Content-MD5", bodyMd5);
+        request.Headers.TryAddWithoutValidation("x-acs-content-sha256", hashedBody);
         request.Headers.TryAddWithoutValidation("x-acs-action", "TranslateGeneral");
         request.Headers.TryAddWithoutValidation("x-acs-signature-nonce", uuid);
-        request.Headers.TryAddWithoutValidation("x-acs-signature-method", "HMAC-SHA1");
         request.Headers.TryAddWithoutValidation("x-acs-version", "2018-10-12");
-        request.Headers.TryAddWithoutValidation("Timestamp", now.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture));
-        request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+        request.Headers.TryAddWithoutValidation("x-acs-date", now.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss'Z'", CultureInfo.InvariantCulture));
+        request.Content = new FormUrlEncodedContent(formData);
         return request;
     }
 }
